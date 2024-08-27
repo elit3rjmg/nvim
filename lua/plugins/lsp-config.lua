2ed38150bfb5124b56ec2a9c0b5ea9fdd010f99a
RJ <84078883+elit3rjmg@users.noreply.github.com>
2024-08-27 15:43:57 +0530

    added ast_grep

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,6 +20,7 @@ return {
         config = function()
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
+            lspconfig.ast_grep.setup({})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({'n', 'v'}, '<space>ca', vim.lsp.buf.code_action, {})
